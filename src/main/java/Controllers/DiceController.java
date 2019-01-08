39f02f781fb90837b9de331492c6532be1ac944d
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2019-01-08 10:47:53 -0600

    finished the dice controller

--- src/main/java/Controllers/DiceController.java
+++ src/main/java/Controllers/DiceController.java
@@ -1,7 +1,9 @@
 package Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,8 +17,17 @@ public class DiceController {
     }
 
     @ResponseBody
-    @GetMapping("/roll-dice/{n}")
-    public String numGuessed (){
+    @GetMapping("/roll-dice/${guess}")
+    public String numGuessedOne (@PathVariable int guess, Model model){
+        int rdmNum = (int) (Math.random() * 6 + 1);
+        model.addAttribute("number", rdmNum);
+        if (rdmNum == guess){
+            model.addAttribute("correct", true);
+        } else {
+            model.addAttribute("correct", false);
+
+        }
         return "dice-rolled";
     }
+
 }
