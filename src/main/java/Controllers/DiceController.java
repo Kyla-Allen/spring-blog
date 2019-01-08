d0f432a3ec3a221c5548de6b7ea32c899dcc832e
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2019-01-08 11:11:16 -0600

    added a page for after the dice is rolled

--- src/main/java/Controllers/DiceController.java
+++ src/main/java/Controllers/DiceController.java
@@ -10,15 +10,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class DiceController {
 
 
-    @ResponseBody
     @GetMapping("/roll-dice")
     public String guessDice (){
         return "roll-dice";
     }
 
-    @ResponseBody
     @GetMapping("/roll-dice/${guess}")
-    public String numGuessedOne (@PathVariable int guess, Model model){
+    public String numGuessed (@PathVariable int guess, Model model){
         int rdmNum = (int) (Math.random() * 6 + 1);
         model.addAttribute("number", rdmNum);
         if (rdmNum == guess){
